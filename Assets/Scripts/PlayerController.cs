15f5ab3f81d9ced366231c42a81d3afe2a7e2270
Tomasz Ziolo <tmkziolo@gmail.com>
2017-04-18 14:36:04 +0200

    Add point system

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
     public float speed;
+    public Text countText;
+    public Text winText;
 
+    private int count;
     private Rigidbody rb;
+
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText();
+        winText.text = "";
     }
 
     void FixedUpdate()
@@ -28,6 +37,16 @@ public class PlayerController : MonoBehaviour {
         if(other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
+            count += 1;
+
+            SetCountText();
         }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 8)
+            winText.text = "You win!";
     }
 }
